8b5316ba5bef1299b142049181e99be28c481a74
Samuel <finseyconrymurray@gmail.com>
2024-03-05 11:24:08 -0500

    Minor changes

--- asm/build.sh
+++ asm/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # Builds the program and writes into the text file for testing
 nasm -f elf64 -g -F dwarf todo.asm -o td.o && ld -o todo td.o
-printf "Test1\nTest2\nTest3\n" > todo.txt
+printf "Test 1\nTest 2\nTest 3" > todo.txt
